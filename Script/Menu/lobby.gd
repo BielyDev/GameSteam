1028729e698fd0d216546c983b079b6203e98456
Bielydev <gabrielgameplay204@gmail.com>
2025-03-10 06:40:58 -0300

    Att

--- Script/Menu/lobby.gd
+++ Script/Menu/lobby.gd
@@ -11,7 +11,23 @@ func _ready() -> void:
 	Steam.lobby_joined.connect(lobby_joined)
 
 func lobby_joined(_lobby_id: int, _permission: int, _block: bool, _responde: int) -> void:
-	Server.notif("Enter lobby!")
+	
+	match _responde:
+		Steam.RESULT_OK:
+			Server.notif("Enter lobby!")
+			return
+		Steam.RESULT_FAIL:
+			Server.notif("Aconteceu algo inesperado! COD 2")
+			return
+		Steam.RESULT_ACCESS_DENIED:
+			Server.notif("Acesso negado!")
+			return
+		Steam.RESULT_CONNECT_FAILED:
+			Server.notif("Conexão falhou!")
+			return
+	Server.notif(str("ERROR ,",_responde))
+	
+	print(" - ",Steam.getNumLobbyMembers(_lobby_id)," - ",_responde)
 
 func lobby_data_update(_lobby_id: int,_changed_id: int,_making_change_id: int) -> void:
 	print(_changed_id)
