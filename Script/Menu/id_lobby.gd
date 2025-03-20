0457a96ac6974df86033cb6da5297a93442d9e39
Bielydev <gabrielgameplay204@gmail.com>
2025-03-20 16:14:52 -0300

    att

--- Script/Menu/id_lobby.gd
+++ Script/Menu/id_lobby.gd
@@ -5,7 +5,10 @@ extends Control
 
 
 func _on_id_text_changed(new_text: String) -> void:
+	var save_caret: int = Id.caret_column
 	Id.text = str(new_text.to_int())
+	Id.caret_column = save_caret
+	
 	Enter.disabled = !Steam.isLobby(int(Id.text))
 
 
