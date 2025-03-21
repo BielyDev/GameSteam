4e38482b28420b49c119c64cba6c7deb84adb972
Bielydev <gabrielgameplay204@gmail.com>
2025-03-21 17:51:53 -0300

    att

--- Script/Player/player.gd
+++ Script/Player/player.gd
@@ -10,6 +10,7 @@ const MAX_GRAVITY: int = 25
 @onready var Camera: Camera3D = $Cam/Camera
 @onready var Navigation: NavigationAgent3D = $Navigation
 @onready var MouseRay: RayCast3D = $Cam/Camera/MouseRay
+@onready var SendPosition: Timer = $Timers/SendPosition
 
 var motion: Vector3
 var speed: float = 2.5
@@ -21,6 +22,8 @@ func _ready() -> void:
 	
 	if !authority:
 		P2P.received_position.connect(sync_pos)
+	else:
+		SendPosition.start()
 
 
 
@@ -30,18 +33,21 @@ func _physics_process(_delta: float) -> void:
 	move_and_slide()
 
 func _moviment() -> void:
+	velocity.x = 0
+	velocity.z = 0
+	
 	if Input.is_action_just_pressed("walk"):
 		pass
 	if Input.is_action_pressed("walk"):
 		Navigation.target_position = MouseRay.get_collision_point()
 	
-	var next_position: Vector3 = global_position.direction_to(Navigation.get_next_path_position()).normalized()
+	var next_position: Vector3 = global_position.direction_to(Navigation.get_next_path_position()).normalized() * speed
 	
-	velocity.x = next_position.x * speed
-	velocity.z = next_position.z * speed
+	velocity.x = next_position.x
+	velocity.z = next_position.z
 	
-	if velocity.x != 0 or velocity.z != 0:
-		P2P.send_position(global_position)
+	print((velocity.x < -0.1 or velocity.x > 0.1) or (velocity.z < -0.1 or velocity.z > 0.1))
+	SendPosition.paused = !(velocity.x < -0.1 or velocity.x > 0.1) or (velocity.z < -0.1 or velocity.z > 0.1)
 
 func _gravity() -> void:
 	velocity.y += -GRAVITY
@@ -55,3 +61,7 @@ func _gravity() -> void:
 func sync_pos(id: int, position: Vector3) -> void:
 	if id == name.to_int() and !authority:
 		global_position = position
+
+
+func _on_send_position_timeout() -> void:
+	P2P.send_position(global_position)
--- Script/Singleton/p2p.gd
+++ Script/Singleton/p2p.gd
@@ -7,9 +7,6 @@ enum PLAYER {
 }
 
 
-func _ready() -> void:
-	Steam.p2p_session_request.connect(p2p_message)
-
 func _process(delta: float) -> void:
 	var events: Dictionary = Steam.readP2PPacket(32,0)
 	if events.size() > 0:
@@ -23,15 +20,11 @@ func _process(delta: float) -> void:
 				
 				received_position.emit(events.remote_steam_id, position)
 
-func p2p_message(_user_id: int) -> void:
-	print("UE",_user_id)
-	print(Steam.readP2PPacket(_user_id))
-
 func send_position(global_position: Vector3) -> void:
 	for peer: int in Steam.getNumLobbyMembers(Lobby.lobby_id):
 		var player_id: int = Steam.getLobbyMemberByIndex(Lobby.lobby_id, peer)
 		
-		send_message(player_id, PLAYER.POSITION, [global_position], Steam.P2P_SEND_UNRELIABLE_NO_DELAY)
+		send_message(player_id, PLAYER.POSITION, [global_position], Steam.P2P_SEND_UNRELIABLE)
 
 func send_message(_send_player: int,_type: int, _message: Array, _flag: int = Steam.P2P_SEND_RELIABLE, _channel: int = 0) -> void:
 	var message: Array = []
