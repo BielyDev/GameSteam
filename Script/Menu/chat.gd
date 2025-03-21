66efd2bebb6a3f93109a5ce9015905643cc4147b
Bielydev <gabrielgameplay204@gmail.com>
2025-03-21 04:07:02 -0300

    att

--- Script/Menu/chat.gd
+++ Script/Menu/chat.gd
@@ -4,6 +4,15 @@ extends PanelContainer
 @onready var Send: ButtonAnimated = $vbox/Bar/Send
 @onready var Vbox: VBoxContainer = $vbox/Scroll/vbox
 
+var image: Array = [
+	preload("res://Assets/2D/Chat/Cat/cat_0.jpeg"),
+	preload("res://Assets/2D/Chat/Cat/cat_1.jpeg"),
+	preload("res://Assets/2D/Chat/Cat/cat_2.jpeg"),
+	preload("res://Assets/2D/Chat/Cat/cat_3.jpeg"),
+	preload("res://Assets/2D/Chat/Cat/cat_4.jpg"),
+]
+
+
 func _ready() -> void:
 	Lobby.lobby_chat.connect(_received_message)
 
@@ -18,11 +27,30 @@ func _commands() -> bool:
 					child.queue_free()
 				message_commands("Chat limpo!")
 				return true
+			"/image":
+				Lobby.send_message(Lobby.MESSAGE_LOBBY.CHAT, [MessageEdit.text])
+				message_commands("Mandando imagem...")
+				return true
 		
 		message_commands(str("O comando ",command[0]," não existe!"))
 		return true
 	
 	return false
+
+
+func _received_commands(_user_id: int, _message: String) -> bool:
+	if _message[0] == "/":
+		var command: PackedStringArray = _message.split(" ")
+		
+		match command[0]:
+			"/image":
+				instance_image(_user_id, int(_message[_message.length()-1]))
+				
+				return true
+		
+		return true
+	return false
+
 
 func message_commands(_message: String) -> void:
 	instance_rich_label(str("[color=gray]",_message))
@@ -37,14 +65,30 @@ func instance_rich_label(_message: String) -> void:
 	
 	Vbox.add_child(new_rich)
 
+func instance_image(_user_id: int, _image: int) -> void:
+	if !(_image < image.size()): return
+	
+	_received_message(_user_id," ")
+	var new_texture: TextureRect = TextureRect.new()
+	
+	new_texture.expand_mode = TextureRect.EXPAND_IGNORE_SIZE
+	new_texture.size_flags_horizontal = Control.SIZE_SHRINK_BEGIN
+	#new_texture.stretch_mode = TextureRect.STRETCH_KEEP
+	new_texture.custom_minimum_size = Vector2(100.0,100.0)
+	new_texture.texture = image[_image]
+	
+	Vbox.add_child(new_texture)
+
 
 func _received_message(_user_id: int, _message: String) -> void:
+	if _received_commands(_user_id ,_message): return
+	
 	var nickname: String
 	
 	if Host.steam_id == _user_id:
-		nickname = str("[color=yellow]",Steam.getPersonaName(),"[/color]: ")
+		nickname = str("[color=cyan]",Steam.getPersonaName(),"[/color]: ")
 	else:
-		nickname = str("[color=blue]",Steam.getFriendPersonaName(_user_id),"[/color]: ")
+		nickname = str("[color=yellow]",Steam.getFriendPersonaName(_user_id),"[/color]: ")
 	
 	instance_rich_label(str(nickname,_message))
 
@@ -57,3 +101,7 @@ func _on_send_pressed() -> void:
 
 func _on_message_edit_text_changed(_new_text: String) -> void:
 	Send.disabled = !_new_text.length() > 0
+
+func _on_message_edit_text_submitted(_new_text: String) -> void:
+	Send.disabled = !_new_text.length() > 0
+	_on_send_pressed()
