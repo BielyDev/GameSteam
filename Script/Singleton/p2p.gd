636f35ea3df484ee773ac5faedd2dd6dcff77eb2
Bielydev <gabrielgameplay204@gmail.com>
2025-03-21 05:44:22 -0300

    Update p2p.gd

--- Script/Singleton/p2p.gd
+++ Script/Singleton/p2p.gd
@@ -11,7 +11,7 @@ func _ready() -> void:
 	Steam.p2p_session_request.connect(p2p_message)
 
 func _process(delta: float) -> void:
-	var events: Dictionary = Steam.readP2PPacket(1,0)
+	var events: Dictionary = Steam.readP2PPacket(0,0)
 	if events.size() > 0:
 		print("events: ",events)
 		
@@ -31,13 +31,16 @@ func send_position(global_position: Vector3) -> void:
 	for peer: int in Steam.getNumLobbyMembers(Lobby.lobby_id):
 		var player_id: int = Steam.getLobbyMemberByIndex(Lobby.lobby_id, peer)
 		
-		send_message(player_id, PLAYER.POSITION, [global_position.x,global_position.y,global_position.z], Steam.P2P_SEND_UNRELIABLE)
+		send_message(player_id, PLAYER.POSITION, [global_position.x,global_position.y,global_position.z], Steam.P2P_SEND_UNRELIABLE_NO_DELAY)
 
 func send_message(_send_player: int,_type: int, _message: Array, _flag: int = Steam.P2P_SEND_RELIABLE, _channel: int = 0) -> void:
-	var message: Array
+	var message: Array = []
 	message.append(_type)
 	
-	for i in _message:
-		message.append(i)
-	
-	Steam.sendP2PPacket(_send_player, (message), _flag, _channel)
+	#for i in _message:
+	#	message.append(i)
+	message.append(int(_message[0]))
+	message.append(int(_message[1]))
+	message.append(int(_message[2]))
+	print(message)
+	Steam.sendP2PPacket(_send_player, message, _flag, _channel)
