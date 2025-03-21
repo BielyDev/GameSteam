53020c1970038d6bfc67d4572d9dedb608e29324
Bielydev <gabrielgameplay204@gmail.com>
2025-03-21 15:41:02 -0300

    att

--- Script/Player/cam.gd
+++ Script/Player/cam.gd
@@ -3,17 +3,17 @@ extends Node3D
 @export var sensi: float = 0.15
 
 @onready var Camera: Camera3D = $Camera
+@onready var MouseRay: RayCast3D = $Camera/MouseRay
+@onready var pos: MeshInstance3D = $"../Timer/pos"
 
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	pass
 
 func _input(_event: InputEvent) -> void:
 	
 	if _event is InputEventMouseMotion:
-		var mouse: Vector2 = _event.relative * sensi
+		var mouse_pos: Vector3 = Camera.project_position(get_viewport().get_mouse_position(),2)
 		
-		rotation.y += -deg_to_rad(mouse.x)
-		Camera.rotation.x += -deg_to_rad(mouse.y )
-		
-		Camera.rotation_degrees.x = clamp(Camera.rotation_degrees.x,-80,80)
+		MouseRay.look_at(mouse_pos)
+		pos.global_position = MouseRay.get_collision_point()
--- Script/Player/player.gd
+++ Script/Player/player.gd
@@ -8,9 +8,11 @@ const GRAVITY: float = 0.3
 const MAX_GRAVITY: int = 25
 
 @onready var Camera: Camera3D = $Cam/Camera
+@onready var Navigation: NavigationAgent3D = $Navigation
+@onready var MouseRay: RayCast3D = $Cam/Camera/MouseRay
 
 var motion: Vector3
-var speed: Array = [SPEED,RUN_SPEED]
+var speed: float = 2.5
 var authority: bool
 
 func _ready() -> void:
@@ -23,32 +25,29 @@ func _ready() -> void:
 
 func _physics_process(_delta: float) -> void:
 	_moviment()
+	_gravity()
 	move_and_slide()
 
 func _moviment() -> void:
+	if Input.is_action_just_pressed("walk"):
+		pass
+	if Input.is_action_pressed("walk"):
+		Navigation.target_position = MouseRay.get_collision_point()
+	
+	var next_position: Vector3 = global_position.direction_to(Navigation.get_next_path_position()).normalized()
+	
+	velocity.x = next_position.x * speed
+	velocity.z = next_position.z * speed
+
+func _gravity() -> void:
 	velocity.y += -GRAVITY
 	
-	if velocity.y < -MAX_GRAVITY:
-		velocity.y = -MAX_GRAVITY
-	
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP
-	
-	motion = Vector3()
-	
-	motion.z += Input.get_axis("right","left") * Camera.global_basis.z.x
-	motion.x += -Input.get_axis("right","left") * Camera.global_basis.z.z
-	motion.z += -Input.get_axis("down","up") * Camera.global_basis.z.z
-	motion.x += -Input.get_axis("down","up") * Camera.global_basis.z.x
-	
-	if motion != Vector3() and authority:
-		P2P.send_position(global_position)
-	
-	motion = motion.normalized() * speed[int(Input.is_action_pressed("run"))]
-	
-	velocity.x = lerp(velocity.x, motion.x, ACCELERATE)
-	velocity.z = lerp(velocity.z, motion.z, ACCELERATE)
+	if is_on_floor():
+		velocity.y = 0
+	else:
+		if velocity.y < -MAX_GRAVITY:
+			velocity.y = -MAX_GRAVITY
 
 func sync_pos(id: int, position: Vector3) -> void:
-	if !authority:
+	if id == name.to_int() and !authority:
 		global_position = position
